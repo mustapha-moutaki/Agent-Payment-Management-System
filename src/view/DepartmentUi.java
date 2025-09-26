7ba55e720922effa5aa3198c46fb181454636724
mustapha <mustaphastar067@gmail.com>
2025-09-26 14:43:39 +0100

    feat: complete the crud of the agent-payment-department controller + agent ui

--- src/view/DepartmentUi.java
+++ src/view/DepartmentUi.java
@@ -1,4 +1,31 @@
 package view;
 
+import controller.MainController;
+import model.Department;
+
+import java.util.List;
+
 public class DepartmentUi {
+    private MainController controller;
+
+    public void addDepartment(Department department){
+        controller.addDepartment(department);
+    }
+    public void deleteDepartment(int id){
+        controller.deleteDepartment(id);
+    }
+    public void updateDepartment(Department department){
+        controller.updateDeaprtment(department);
+    }
+    public void getDepartmentsList(){
+        List<Department> departmentsList = controller.DepartmentList();
+        System.out.println("++++++++++++Departments List++++++++++++++++");;
+        for(Department department : departmentsList){
+            System.out.println("=====> Department Name: "+ department.getName());
+        }
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+    }
+    public void getDepartmentById(int id){
+        controller.getDepartmentByid(id);
+    }
 }
--- src/view/PaymentUi.java
+++ src/view/PaymentUi.java
@@ -1,4 +1,31 @@
 package view;
 
+import controller.MainController;
+import model.Payment;
+
+import java.util.List;
+
 public class PaymentUi {
+
+    private MainController controller;
+
+
+    public void addPayment(Payment payment){
+        controller.addPayment(payment);
+    }
+    public void deletePayment(int id){
+        controller.deletePayment(id);
+    }
+    public void updatePayment(Payment payment){
+        controller.updatePayment(payment);
+    }
+    public void paymentsList(){
+        List<Payment> paymentList = controller.paymentList();
+        for(Payment payment : paymentList){
+            System.out.println("payment type: "+ payment.getType().name());
+            System.out.println("payment amount: "+ payment.getAmount());
+            System.out.println("payment name: "+ payment.getCondition_validated());
+        }
+    }
+
 }
