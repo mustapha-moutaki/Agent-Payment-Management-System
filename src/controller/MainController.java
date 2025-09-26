94f6bf7adbc1218ae2bdeac4b6449899ef65aadd
mustapha <mustaphastar067@gmail.com>
2025-09-26 14:40:42 +0100

    feat: complete the crud of the agent-payment-department controller + agent ui

---
+++ src/controller/MainController.java
@@ -0,0 +1,126 @@
+package controller;
+
+import com.sun.tools.javac.Main;
+import model.Agent;
+import model.Department;
+import model.Payment;
+import service.AgentService;
+import service.DepartmentService;
+import service.PaymentService;
+
+import java.util.List;
+
+// how to create a controller that handle all services (busines logic of all models)
+public class MainController {
+    /**
+     * here we import all Services we have
+     */
+    private AgentService agentService;
+    private DepartmentService departmentService;
+    private PaymentService paymentService;
+
+    /* constructor
+     */
+
+    public MainController(AgentService agentService, DepartmentService departmentService, PaymentService paymentService){
+        this.agentService = agentService;
+        this.departmentService = departmentService;
+        this.paymentService = paymentService;
+    }
+
+    /**
+     *
+     * ==================== agent controller =========================
+     */
+    // 1- create agent
+    public void addAgent(Agent agent){
+        agentService.addAgent(agent);
+    }
+
+    // 2- delete agent
+    public void deleteAgent(int id){
+        agentService.deleteAgent(id);
+    }
+
+    // 3-update an agent
+    public void updateAgent(Agent agent){
+        agentService.updateAgent(agent);
+    }
+
+    // 4-get all agent list
+    public List<Agent> getAllAgent(){
+        return agentService.getAllAgents();
+    }
+
+    // 5- get agent by id
+    public Agent getAgent(int id){
+        return agentService.getAgentById(id);
+    }
+
+
+    /**
+     * Department Controller
+     */
+
+    // 1- Create Department
+    public void addDepartment(Department department){
+        departmentService.addDepartment(department);
+    }
+
+    // 2- delete department
+    public void deleteDepartment(int id){
+        departmentService.deleteDepartment(id);
+    }
+
+    // 3- update department
+    public void updateDeaprtment(Department department){
+        departmentService.updateDepartment(department);
+    }
+
+    // 4-Get list of departments
+    public List<Department>DepartmentList(){
+        return departmentService.findAll();
+    }
+
+    // 5- get departmnt by id
+    public Department getDepartmentByid(int id){
+        return departmentService.findDepartmentById(id);
+    }
+
+
+    /**
+     * Payment Controller
+     */
+
+    // 1- create Payment
+    public void addPayment(Payment payment){
+        paymentService.addPayment(payment);
+    }
+
+    // 2- delete Payment
+    public void deletePayment(int id){
+        paymentService.deletePayment(id);
+    }
+
+    // 3- update Payment
+    public void updatePayment(Payment payment){
+         paymentService.updatePayment(payment);
+    }
+
+    // 4- Get payments list
+    public List<Payment>paymentList(){
+        return paymentService.getAll();
+    }
+
+    // 5- get payment by id
+    public Payment getPayment(int id){
+        return paymentService.findPaymentById(id);
+    }
+
+
+    /**
+     * done
+     */
+
+
+}
--- src/view/AgentUi.java
+++ src/view/AgentUi.java
@@ -1,4 +1,97 @@
 package view;
 
+import controller.MainController;
+import model.Agent;
+import model.Department;
+import model.Payment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AgentUi {
+    private MainController controller;
+
+    public AgentUi(MainController controller){
+        this.controller = controller;
+    }
+
+
+    public void addAgent(Agent agent){
+        controller.addAgent(agent);
+    }
+
+    public void deleteAgent(int id){
+        controller.deleteAgent(id);
+    }
+    public void updateAgent(Agent agent){
+        controller.updateAgent(agent);
+    }
+    public Agent getAgentById(int id){
+        return controller.getAgent(id);
+    }
+    public void getAgentList(){
+        List<Agent>agentList = controller.getAllAgent();
+        System.out.println("++++++++++++Agent info++++++++++++++++");
+        for(Agent agent: agentList){
+            System.out.println("<------------------------->");
+            System.out.println("first name: "+ agent.getFirst_name());
+            System.out.println("first name: "+ agent.getLast_name());
+            System.out.println("first name: "+ agent.getEmail());
+            System.out.println("first name: "+ agent.getAgent_type().name());// it's enum
+            System.out.println("<------------------------->");
+//            System.out.println("first name: "+ agent.getFirst_name());using joins to return his department
+        }
+        System.out.println("+++++++++++++++++++++++++++++++++++");
+    }
+
+
+
+
+
+
+    public void addDepartment(Department department){
+        controller.addDepartment(department);
+    }
+    public void deleteDepartment(int id){
+        controller.deleteDepartment(id);
+    }
+    public void updateDepartment(Department department){
+        controller.updateDeaprtment(department);
+    }
+    public void getDepartmentsList(){
+        List<Department> departmentsList = controller.DepartmentList();
+        System.out.println("++++++++++++Departments List++++++++++++++++");;
+        for(Department department : departmentsList){
+            System.out.println("=====> Department Name: "+ department.getName());
+        }
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+    }
+    public void getDepartmentById(int id){
+        controller.getDepartmentByid(id);
+    }
+
+
+
+
+
+    public void addPayment(Payment payment){
+        controller.addPayment(payment);
+    }
+    public void deletePayment(int id){
+        controller.deletePayment(id);
+    }
+    public void updatePayment(Payment payment){
+        controller.updatePayment(payment);
+    }
+    public void paymentsList(){
+        List<Payment> paymentList = controller.paymentList();
+        for(Payment payment : paymentList){
+            System.out.println("payment type: "+ payment.getType().name());
+            System.out.println("payment amount: "+ payment.getAmount());
+            System.out.println("payment name: "+ payment.getCondition_validated());
+        }
+    }
+
+
+
 }
