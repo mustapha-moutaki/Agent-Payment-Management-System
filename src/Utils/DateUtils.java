6c2ff25a1de84402a5b68352134a9719e4eea632
mustapha <mustaphastar067@gmail.com>
2025-09-28 16:36:49 +0100

    feat(DateUtils): complete DateUtils features st 2--

--- src/Utils/DateUtils.java
+++ src/Utils/DateUtils.java
@@ -1,4 +1,27 @@
 package Utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtils {
+    private static final String String DEFAULT_PATTERN = "dd/MM/yyyy";
+    private static final SimpleDateFormat sdf = new SimpleDateFormat(DEFAULT_PATTERN);
+
+    public  static String format(Date date){
+        return sdf.format(date);
+    }
+
+    public static Date parse(String dateString){
+        try{
+            return sdf.parse(dateString);
+        }catch(ParseException e){
+            System.out.println("==> invalid date format, pls try use "+ DEFAULT_PATTERN);
+            return null;
+        }
+    }
 }
+
+/**
+ * usage DAte paymentDAte = DateUtils.parse("10/01/2023")
+ */
